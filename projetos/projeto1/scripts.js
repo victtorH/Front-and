b53283a0f5ee68235f4f5db964b49d89bac4e668
Victtor Hugo Estevam <estevam.victtor@gmail.com>
2024-07-23 06:15:39 -0300

    sim

--- projetos/projeto1/scripts.js
+++ projetos/projeto1/scripts.js
@@ -1,19 +1,19 @@
 const nav= document.querySelector(' .nav-bar')
 const opi = document.querySelector('nav')
 const h2 = document.querySelector('h2')
+
+
 var conta=1
 var soma=0
-opi.addEventListener("click", function clque(){
+opi.addEventListener("click", function (){
     if(soma < conta){
-    h2.style = '  opacity: 0;'
-    opi.style = ' right: 1em; '
+    h2.style = '  font-size:2em;'
+    opi.style = ' right: 0em ;; '
     soma++ 
 }else{
    
- h2.style = '  opacity: 1;'
-opi.style = ' right: -7em; '
-
+ h2.style = '  font-size:1.5em;'
+opi.style = ' right: -7em;  background-color: transparent;  '
 conta++
     }
-
 })
